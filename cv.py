09e5f911f96ffe127380ad134f2d3756fc9fc793
Shikii_ <33571437+Shikiiii@users.noreply.github.com>
2020-02-22 11:11:59 +0200

    Update cv.py

--- cv.py
+++ cv.py
@@ -6,3 +6,5 @@ import requests
 import json
 
 bot = commands.Bot(command_prefix="*", case_insensitive=True)
+
+colorsEmbed = ["0xFF2121", "0xF4FF21", "0x9BFF21", "0x42FF21", "0x21FFCB", "0x21AEFF", "0x3F21FF", "0xBC21FF", "0xFF219B", "0x000000", "0xFFFFFF"]
