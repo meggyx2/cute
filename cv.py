ac2e0df5bcc3ce78fd59e373da4020b450cb32df
Shikii_ <33571437+Shikiiii@users.noreply.github.com>
2020-02-22 10:24:38 +0200

    Create cv.py

---
+++ cv.py
@@ -0,0 +1,8 @@
+from discord.ext.commands import Bot
+from discord.ext import commands
+import discord
+import http.client
+import requests
+import json
+
+bot = commands.Bot(command_prefix="*", case_insensitive=True)
