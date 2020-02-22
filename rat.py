c1b5b7c873dbe674127b947e6ec2727263f4246d
Shikii_ <33571437+Shikiiii@users.noreply.github.com>
2020-02-22 10:21:48 +0200

    Create rat.py

---
+++ rat.py
@@ -0,0 +1,13 @@
+import discord
+from discord import Message, Guild, Member
+from typing import Optional
+from discord.ext import commands
+import os
+import sys
+import traceback
+import asyncio
+from datetime import datetime
+import datetime
+import random
+import json
+import requests
