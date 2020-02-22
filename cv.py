6fc6c6518544bbb519216740ddc8835c55ed0622
Shikii_ <33571437+Shikiiii@users.noreply.github.com>
2020-02-22 12:42:26 +0200

    Update cv.py

--- cv.py
+++ cv.py
@@ -7,4 +7,4 @@ import json
 
 bot = commands.Bot(command_prefix="*", case_insensitive=True)
 
-colorsEmbed = ["0xFF2121", "0xF4FF21", "0x9BFF21", "0x42FF21", "0x21FFCB", "0x21AEFF", "0x3F21FF", "0xBC21FF", "0xFF219B", "0x000000", "0xFFFFFF"]
+colorsEmbed = ["FF2121", "F4FF21", "9BFF21", "42FF21", "21FFCB", "21AEFF", "3F21FF", "BC21FF", "FF219B", "000000", "FFFFFF"]
