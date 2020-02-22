39b39b9a00d569def6f8f39bd9dabec1167a3fad
Shikii_ <33571437+Shikiiii@users.noreply.github.com>
2020-02-22 11:44:55 +0200

    Update bc.py

--- commands/bc.py
+++ commands/bc.py
@@ -12,12 +12,12 @@ from cv.py import *
 @bot.command(aliases=["botclear", "botclean", "clean", "clear", "fuckoffbots"])
 @commands.has_permissions(manage_messages=True)
 async def bc(ctx):
-    def check(m):
-        return m.author.bot
-    await ctx.message.delete()
-    await ctx.message.channel.purge(limit=100, check=check)
+	def check(m):
+		return m.author.bot
+	await ctx.message.delete()
+	await ctx.message.channel.purge(limit=100, check=check)
 	# under this is just for fun
-    def check2(m):
-    	return m.channel == ctx.message.channel
+	def check2(m):
+		return m.channel == ctx.message.channel
 	msg = await client.wait_for('message', check=check)
 	await msg.add_reaction("<a:speedclean:653273382812647425>")
