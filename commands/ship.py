87ddb628d1070a0544b6fa77e32473ebea90208f
Shikii_ <33571437+Shikiiii@users.noreply.github.com>
2020-02-25 19:21:32 +0200

    Create ship.py

---
+++ commands/ship.py
@@ -0,0 +1,10 @@
+import discord
+from discord.ext import commands
+import random
+import sys
+import traceback
+import asyncio
+import datetime
+import json
+from datetime import datetime
+from cv import *
