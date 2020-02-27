208ac7690ff68ab1ccf141db4bf25b11b70c01be
Shikii_ <33571437+Shikiiii@users.noreply.github.com>
2020-02-27 20:40:16 +0200

    Create events.py

---
+++ commands/events/events.py
@@ -0,0 +1,13 @@
+import discord
+from discord.ext import commands
+import random
+import sys
+import traceback
+import asyncio
+import datetime
+import json
+from datetime import datetime
+from cv import *
+
+@bot.event
+async def on_message():
