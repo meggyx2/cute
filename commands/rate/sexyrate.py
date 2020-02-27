db9b257e7a02b423957252f8a95622c696991f91
Shikii_ <33571437+Shikiiii@users.noreply.github.com>
2020-02-27 19:50:09 +0200

    Update sexyrate.py

--- commands/rate/sexyrate.py
+++ commands/rate/sexyrate.py
@@ -7,7 +7,7 @@ import random
 from datetime import datetime
 from cv import *
  
-@bot.command(aliases=["howsexy", "howhot"])
+@bot.command(aliases=["howsexy"])
 async def sexyrate(ctx, user: discord.Member):
     await ctx.send(f"**{user.name}** is **__{str(random.randint(0, 100))}__**% sexy! 😉")
 
