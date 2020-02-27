4dd2786c8bd4cab066a902d74cc9a4f80f619dab
Shikii_ <33571437+Shikiiii@users.noreply.github.com>
2020-02-27 17:32:23 +0200

    Update cuterate.py

--- commands/rate/cuterate.py
+++ commands/rate/cuterate.py
@@ -7,7 +7,7 @@ import random
 from datetime import datetime
 from cv import *
 
-@bot.command()
+@bot.command(aliases=["howcute", "howadorable"])
 async def cuterate(ctx, user: discord.Member):
 	await ctx.send(f"**{user.name}** is **__{int(random.randint(0, 100)}__**% cute!")
 
