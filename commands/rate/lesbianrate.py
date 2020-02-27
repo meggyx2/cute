8bb1a0fb28bb1b0d0d75057c7faa33aa27fa4f6f
Shikii_ <33571437+Shikiiii@users.noreply.github.com>
2020-02-27 20:10:59 +0200

    Update lesbianrate.py

--- commands/rate/lesbianrate.py
+++ commands/rate/lesbianrate.py
@@ -9,12 +9,12 @@ from cv import *
 
 @bot.command()
 async def lesbianrate(ctx, user: discord.Member):
-	await ctx.send(f"**{user.name}** is **__{int(random.randint(0, 100)}__**% lesbian! 🏳️‍🌈")
+	await ctx.send(f"**{user.name}** is **__{int(random.randint(0, 100))}__**% lesbian! 🏳️‍🌈")
 
 @lesbianrate.error
 async def lesbianrate_error(ctx, error):
 	if isinstance(error, commands.MissingRequiredArgument):
-		await ctx.send(f"**{ctx.message.author.name}** is **__{int(random.randint(0, 100)}__**% lesbian! 🏳️‍🌈")
+		await ctx.send(f"**{ctx.message.author.name}** is **__{int(random.randint(0, 100))}__**% lesbian! 🏳️‍🌈")
 	elif isinstance(error, commands.BadArgument):
 		embed = discord.Embed(color=0xFFFFFF)
 		embed.set_footer(text="Member not found.")
