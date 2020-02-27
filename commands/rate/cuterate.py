10292db7a8e6ddee3abd6f564965e2c2ee42e494
Shikii_ <33571437+Shikiiii@users.noreply.github.com>
2020-02-27 17:09:22 +0200

    Create cuterate.py

---
+++ commands/rate/cuterate.py
@@ -0,0 +1,26 @@
+import discord
+from discord.ext import commands
+import traceback
+import datetime
+import asyncio
+import random
+from datetime import datetime
+from cv import *
+
+@bot.command()
+async def cuterate(ctx, user: discord.Member):
+	await ctx.send(f"**{user.name}** is **__{int(random.randint(0, 100)}__**% cute!")
+
+@cuterate.error
+async def cuterate_error(ctx, error):
+	if isinstance(error, commands.MissingRequiredArgument):
+		await ctx.send(f"**{ctx.message.author.name}** is **__{int(random.randint(0, 100)}__**% cute!")
+	elif isinstance(error, commands.BadArgument):
+		embed = discord.Embed(color=0xFFFFFF)
+		embed.set_footer(text="Member not found.")
+		await ctx.send(f"**babi** is **__1000__**% cute, duh.", embed=embed)
+	else:
+		print('Ignoring exception in command av:', file=sys.stderr)
+		traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
+		embed = discord.Embed(description="{}".format(error), color=0x000000)
+		await ctx.send("uh, oops, i guess?", embed=embed)
