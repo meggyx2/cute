d83e8ca2507ec7767252c5a3ced694e0b19afd41
Shikii_ <33571437+Shikiiii@users.noreply.github.com>
2020-02-27 21:08:48 +0200

    Create afk.py

---
+++ commands/afk.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+import random
+import sys
+import traceback
+import asyncio
+import datetime
+import json
+from datetime import datetime
+from cv import *
+
+@bot.command()
+async def afk(ctx):
+	if ctx.message.author.display_name.startswith("[AFK]"):
+		return
+	else:
+		await ctx.message.author.edit(nick="[AFK] {}".format(ctx.message.author.name))
+		await ctx.send(f"**{ctx.message.author.name}** is now AFK. We'll be waiting for your return! I'll notify anyone who mentions you.")
