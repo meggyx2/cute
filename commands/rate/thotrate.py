0079dbe7dcad7faab598b5e4641c2e82fe631574
Shikii_ <33571437+Shikiiii@users.noreply.github.com>
2020-02-27 19:58:50 +0200

    Update thotrate.py

--- commands/rate/thotrate.py
+++ commands/rate/thotrate.py
@@ -7,18 +7,18 @@ import random
 from datetime import datetime
 from cv import *
 
-@bot.command(aliases=["howwhore", "howthot", "whorerate", "hoerate", "howhoe"])
+@bot.command(aliases=["howwhore", "howthot", "whorerate", "hoerate", "howhoe", "ratethot"])
 async def thotrate(ctx, user: discord.Member):
-	await ctx.send(f"**{user.name}** is **__{int(random.randint(0, 100))}__**% a thot!")
+	await ctx.send(f"**{user.name}** is **__{int(random.randint(0, 100))}__**% a thot! 🥴")
 
 @thotrate.error
 async def thotrate_error(ctx, error):
 	if isinstance(error, commands.MissingRequiredArgument):
-		await ctx.send(f"**{ctx.message.author.name}** is **__{int(random.randint(0, 100))}__**% a thot!")
+		await ctx.send(f"**{ctx.message.author.name}** is **__{int(random.randint(0, 100))}__**% a thot! 🥴")
 	elif isinstance(error, commands.BadArgument):
 		embed = discord.Embed(color=0xFFFFFF)
 		embed.set_footer(text="Member not found.")
-		await ctx.send(f"**babi** is **__not__** a thot, duh. She's loyal to ~~Meg~~ Shiki. 😳", embed=embed)
+		await ctx.send(f"**babi** is **__not__** a thot, duh. She's loyal to Meg. 😳", embed=embed)
 	else:
 		print('Ignoring exception in command av:', file=sys.stderr)
 		traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
