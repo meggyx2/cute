7e9d2231b4370d72fed3eaaf0f8e6f7617b08fbb
Shikii_ <33571437+Shikiiii@users.noreply.github.com>
2020-02-27 17:09:43 +0200

    Create thotrate.py

---
+++ commands/rate/thotrate.py
@@ -0,0 +1,26 @@
+import discord
+from discord.ext import commands
+import traceback
+import datetime
+import asyncio
+import random
+from datetime import datetime
+from cv import *
+
+@bot.command()
+async def thotrate(ctx, user: discord.Member):
+	await ctx.send(f"**{user.name}** is **__{int(random.randint(0, 100)}__**% a thot!")
+
+@cuterate.error
+async def thotrate_error(ctx, error):
+	if isinstance(error, commands.MissingRequiredArgument):
+		await ctx.send(f"**{ctx.message.author.name}** is **__{int(random.randint(0, 100)}__**% a thot!")
+	elif isinstance(error, commands.BadArgument):
+		embed = discord.Embed(color=0xFFFFFF)
+		embed.set_footer(text="Member not found.")
+		await ctx.send(f"**babi** is **__not__** a thot, duh. She's loyal to ~~Meg~~ Shiki. 😳", embed=embed)
+	else:
+		print('Ignoring exception in command av:', file=sys.stderr)
+		traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
+		embed = discord.Embed(description="{}".format(error), color=0x000000)
+		await ctx.send("uh, oops, i guess?", embed=embed)
