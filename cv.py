009dfad06c509cde1e8af735161e4bbee2771103
Shikii_ <33571437+Shikiiii@users.noreply.github.com>
2020-02-29 11:36:48 +0200

    Update cv.py

--- cv.py
+++ cv.py
@@ -8,6 +8,8 @@ import json
 bot = commands.Bot(command_prefix="*", case_insensitive=True)
 
 snipe_msgs = {}
+snipe_msgs_time = {}
 editsnipe_msgs = {}
+editsnipe_msgs_time = {}
 
 colorsEmbed = ["FF2121", "F4FF21", "9BFF21", "42FF21", "21FFCB", "21AEFF", "3F21FF", "BC21FF", "FF219B", "000000", "FFFFFF"]
