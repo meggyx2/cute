d76c590589a362c86488934017f9d331e6424d75
Shikii_ <33571437+Shikiiii@users.noreply.github.com>
2020-02-29 11:48:43 +0200

    Create editsnipe.py

---
+++ commands/editsnipe.py
@@ -0,0 +1,27 @@
+import discord
+from discord.ext import commands
+import random
+import sys
+import traceback
+import asyncio
+import datetime
+import json
+from datetime import datetime
+from cv import *
+
+@bot.command()
+async def editsnipe(ctx):
+	try:
+		to_slice = editsnipe_msgs[str(ctx.message.channel.id)]
+		sniping = to_slice.split("|")
+	except:
+		await ctx.send(f"**{ctx.message.author.display_name}** there's nothing to snipe??")
+		return
+	try:
+		author = ctx.guild.get_member(int(sniping[1]))
+	except:
+		await ctx.send(f"**{ctx.message.author.display_name}** uh, it looks like the author of the message you're trying to snipe left the server. L")
+		return
+	embed = discord.Embed(description="{}".format(sniping[0]), color=0xFFFFFF, timestamp=editsnipe_msgs_time[str(ctx.message.channel.id)])
+	embed.set_author(name="{}".format(author.name), icon_url=author.avatar_url)
+	await ctx.send(embed=embed)
