5e3e36a4d68d3522648d299001d3abc8e4902ab9
Shikii_ <33571437+Shikiiii@users.noreply.github.com>
2020-07-04 16:18:58 +0300

    Update afk.py

--- commands/afk.py
+++ commands/afk.py
@@ -14,8 +14,12 @@ async def afk(ctx):
 	if ctx.message.author.display_name.startswith("[AFK]"):
 		return
 	else:
-		await ctx.send(f"**{ctx.message.author.display_name}** is now AFK. We'll be waiting for your return! I'll notify anyone who mentions you.")
 		try:
 			await ctx.message.author.edit(nick="[AFK] {}".format(ctx.message.author.display_name))
 		except:
 			print(f"Couldn't change {ctx.message.author.display_name}'s name because it's too long (32 max char).")
+			embed = discord.Embed(description="I couldn't activate your AFK status because your nickname is too long. (MAX: 32 chars)\nPlease, **make your nickname shorter** and try again.")
+			await ctx.send(f"{ctx.message.author.mention}", embed=embed)
+			return
+		await ctx.send(f"**{ctx.message.author.display_name}** is now AFK. We'll be waiting for your return! I'll notify anyone who mentions you.")
+
